1b20939eaa11fd1eb721e621ad3675c212953ab6
GlimpseMumble <easysay666@gmail.com>
2025-02-24 08:03:35 -0600

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # Usage: ./rename_app.sh NewName OriginalAppName
-# $1 = the new name (e.g., EvanTest)
+# $1 = the new name (e.g., New_App_Name)
 # $2 = the original app's name (e.g., DuckDuckGo, without .app extension)
 
-# Rename the .app bundle
+# rename the .app bundle
 mv "$2.app" "$1.app"
 
-# Rename the executable inside Contents/MacOS
+# rename the runnable inside Contents/MacOS
 mv "$1.app/Contents/MacOS/$2" "$1.app/Contents/MacOS/$1"
 
-# Set the new CFBundleName and CFBundleExecutable values in Info.plist
+# set the new "CFBundleName" and "CFBundleExecutable" values in "Info.plist"
 /usr/libexec/PlistBuddy -c "Set :CFBundleName $1" "$1.app/Contents/Info.plist"
 /usr/libexec/PlistBuddy -c "Set :CFBundleExecutable $1" "$1.app/Contents/Info.plist"
 
-# Re-sign the application to ensure it's recognized as a valid app
+# re-sign the application
 codesign --force --sign - "$1.app"
 
-# Remove the quarentine attribute
+# Remove the quarentine attribute <3
 xattr -d com.apple.quarantine "$1.app"
